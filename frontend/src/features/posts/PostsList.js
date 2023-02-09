736f29fa3dd480a4e0748b270664f86a1389b693
jadilovic <adilovic79@yahoo.com>
2023-02-09 22:37:08 +0100

    used async thunk to fetch data

--- frontend/src/features/posts/PostsList.js
+++ frontend/src/features/posts/PostsList.js
@@ -1,30 +1,51 @@
-import { useSelector } from 'react-redux';
-import { selectAllPosts } from './postsSlice';
-import PostAuthor from './PostAuthor';
-import TimeAgo from './TimeAgo';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+	selectAllPosts,
+	getPostsError,
+	getPostsStatus,
+	fetchPosts,
+} from './postsSlice';
+import { useEffect } from 'react';
+import PostExcerpt from './PostExcerpt';
 
 const PostsList = () => {
+	const dispatch = useDispatch();
+
 	const posts = useSelector(selectAllPosts);
+	const postStatus = useSelector(getPostsStatus);
+	const error = useSelector(getPostsError);
 
-	const orderedPosts = posts
-		.slice()
-		.sort((a, b) => b.date.localeCompare(a.date));
+	useEffect(() => {
+		if (postStatus === 'idle') {
+			dispatch(fetchPosts());
+		}
+	}, [postStatus, dispatch]);
 
-	const renderedPosts = orderedPosts.map((post) => (
-		<article key={post.id}>
-			<h3>{post.title}</h3>
-			<p>{post.content}</p>
-			<p className="postCredit">
-				<PostAuthor userId={post.userId} />
-				<TimeAgo timestamp={post.date} />
-			</p>
-		</article>
-	));
+	// const orderedPosts = posts
+	// 	.slice()
+	// 	.sort((a, b) => b.date.localeCompare(a.date));
 
+	// const renderedPosts = orderedPosts.map((post) => (
+	// 	<PostExcrpt key={post.id} post={post} />
+	// ));
+	let content;
+	if (postStatus === 'loading') {
+		content = <p>"Loading..."</p>;
+	} else if (postStatus === 'succeeded') {
+		const orderedPosts = posts
+			.slice()
+			.sort((a, b) => b.date.localeCompare(a.date));
+		content = orderedPosts.map((post) => (
+			<PostExcerpt key={post.date} post={post} />
+		));
+	} else if (postStatus === 'failed') {
+		content = <p>{error}</p>;
+	}
 	return (
 		<section>
 			<h2>Posts</h2>
-			{renderedPosts}
+			{/* {renderedPosts} */}
+			{content}
 		</section>
 	);
 };
